d0a21f8e130f29cdfe8ed1b81a65c086c65f9645
Simon Ljungberg <hi@iamsim.me>
2014-02-14 10:30:18 +0100

    Start testing Furcape.

---
+++ test/furcape.js
@@ -0,0 +1,43 @@
+var assert  = require('assert');
+var furcape = require('../');
+
+describe('Furcape', function () {
+  it('returns a new object when called as a function', function () {
+    var f = furcape();
+    assert(f.criteria);
+  });
+
+  it('correctly registers a single criteria', function () {
+    var f = furcape();
+    f.registerCriteria(testeria);
+    assert(f.criteria.testeria);
+  });
+
+  it('throws when registering same criteria twice', function () {
+    var f = furcape();
+    f.registerCriteria(testeria);
+
+    assert.throws(function () {
+      f.registerCriteria(testeria);
+    }, /already defined\.$/);
+  });
+
+  it('registers an array of criteria', function () {
+    var f = furcape();
+    f.registerCriteria([testeria, festeria]);
+    assert(f.criteria.testeria);
+    assert(f.criteria.festeria);
+  });
+});
+
+// Mock criteria
+var testeria = function testeria() {
+
+};
+testeria.name = 'testeria';
+
+var festeria = function festeria() {
+
+};
+festeria.name = 'festeria';
+
