6ba0a3cbdde8ebd58155d313b1b0d22daf2f3f48
Simon Ljungberg <hi@iamsim.me>
2014-02-14 10:17:05 +0100

    Forgot we were doing bdd style tests, not tdd. Whatever that means.

--- test/group.js
+++ test/group.js
@@ -1,14 +1,14 @@
 var assert = require('assert');
 var Group  = require('../lib/group');
 
-suite('Group', function () {
-  test('Instantiating a new group sets the UUID', function () {
+describe('Group', function () {
+  it('instantiates new group with UUID', function () {
     var g = new Group({ title: 'foobar' });
     assert(g.uuid);
     assert.equal(g.title, 'foobar');
   });
 
-  test('Correctly deserializes an object', function () {
+  it('correctly deserializes an object', function () {
     var data = {
       title: 'a',
       name: 'b',
@@ -25,7 +25,7 @@ suite('Group', function () {
     assert.equal(g.criteria.d, 'e');
   });
 
-  test('Throws when serializing group without title', function () {
+  it('throws when serializing group without title', function () {
     var data = {
       name: 'b',
       uuid: 'c',
@@ -40,7 +40,7 @@ suite('Group', function () {
     }, /title\.$/);
   });
 
-  test('Throws when serializing group without name', function () {
+  it('throws when serializing group without name', function () {
     var data = {
       title: 'b',
       uuid: 'c',
@@ -55,7 +55,7 @@ suite('Group', function () {
     }, /name\.$/);
   });
 
-  test('Throws when serializing group without UUID', function () {
+  it('throws when serializing group without UUID', function () {
     var data = {
       title: 'b',
       name: 'c',
@@ -69,6 +69,28 @@ suite('Group', function () {
     assert.throws(function () {
       g.serialize();
     }, /uuid\.$/);
+  });
+
+  it('correctly serializes an object', function () {
+    var data = {
+      title: 'a',
+      name: 'b',
+      uuid: 'c',
+      criteria: {
+        d: 'e'
+      }
+    };
+    var g = new Group(data);
+    var obj = g.serialize();
+    assert.equal(obj.title, 'a');
+    assert.equal(obj.name, 'b');
+    assert.equal(obj.uuid, 'c');
+    assert.equal(obj.criteria.d, 'e');
+  });
+
+  it('creates a new group when called as a function', function () {
+    var g = Group();
+    assert(g.uuid);
   });
 });
 
