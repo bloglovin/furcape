f56a45f367f483f27cfe1cecaedf3e6a04faab65
Simon Ljungberg <hi@iamsim.me>
2014-02-17 15:02:50 +0100

    100% code coverage

--- test/furcape.js
+++ test/furcape.js
@@ -116,6 +116,117 @@ describe('Furcape', function () {
       done();
     });
   });
+
+  it('correctly evaluates a subset of groups', function (done) {
+    var f = furcape();
+    f.registerCriteria(compare);
+    f.registerCriteria(equal);
+    f.registerCriteria(percent);
+
+    f.createGroup('foo', 'foo', {
+      compare: {
+        lessThan: { a: 'data.a', b: 'data.b' },
+      }
+    });
+
+    f.createGroup('bar', 'bar', {
+      equality: { a: 'data.c', b: 'group.name' }
+    });
+
+    f.createGroup('percent', 'percent', {
+      percentage: {
+        hashProps: ['a', 'group.name'],
+        ranges: {
+          a: { min: 0, max: 50 },
+          b: { min: 50.000001, max: 100 }
+        }
+      }
+    });
+
+    f.evaluate({
+      a: 1337,
+      b: '80085',
+      c: 'bar'
+    }, ['foo', 'bar'], function (err, results) {
+      assert(!err);
+      assert(results.indexOf('foo') !== -1);
+      assert(results.indexOf('bar') !== -1);
+      assert(results.indexOf('b') === -1);
+      done();
+    });
+  });
+
+  it('throws an error when creating a group with existing name', function () {
+    var f = furcape();
+    f.createGroup('bar', 'bar', {});
+    assert.throws(function () {
+      f.createGroup('bar', 'bar', {});
+    }, /Group (.+) already exists\./);
+  });
+
+  it('returns an error for invalid groups', function (done) {
+    var f = furcape();
+    f.createGroup('foo', 'foo', {
+      compare: {
+        lessThan: { a: 'data.a', b: 'data.b' },
+      }
+    });
+
+    f.evaluateGroup({}, 'foo', function (err, result) {
+      assert(err);
+      assert(!result);
+      done();
+    });
+  });
+
+  it('correctly catches failing tests', function (done) {
+    var f = furcape();
+    f.registerCriteria(compare);
+    f.registerCriteria(equal);
+    f.registerCriteria(percent);
+
+    f.createGroup('foo', 'foo', {
+      compare: {
+        lessThan: { x: 'data.x', b: 'data.b' },
+      }
+    });
+
+    f.evaluate({
+      a: 1337,
+      b: 'invalid number',
+      c: 'bar'
+    }, function (err, results) {
+      assert(err);
+      done();
+    });
+  });
+
+  it('correctly excludes failing groups', function (done) {
+    var f = furcape();
+    f.registerCriteria(compare);
+    f.registerCriteria(equal);
+
+    f.createGroup('foo', 'foo', {
+      compare: {
+        lessThan: { a: 'data.b', b: 'data.a' },
+      }
+    });
+
+    f.createGroup('bar', 'bar', {
+      equality: { a: 'data.c', b: 'group.name' }
+    });
+
+    f.evaluate({
+      a: 1337,
+      b: '80085',
+      c: 'bar'
+    }, function (err, results) {
+      assert(!err);
+      assert(results.indexOf('foo') === -1);
+      assert(results.indexOf('bar') !== -1);
+      done();
+    });
+  });
 });
 
 // Mock criteria
